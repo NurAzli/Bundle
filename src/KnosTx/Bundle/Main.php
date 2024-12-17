f7bc777eb850609e33001db900c701e00309ad66
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-12-17 22:01:13 +0700

    Create Main.php

---
+++ src/KnosTx/Bundle/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KnosTx\Bundle;
+
+use KnosTx\Bundle\item\Bundle;
+use pocketmine\plugin\PluginBase;
+use pocketmine\item\VanillaItems;
+
+class Main extends PluginBase{
+    public function onEnable(): void{
+        VanillaItems::registerItem(new Bundle(), true);
+    }
+}
